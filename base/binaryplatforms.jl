c0b3f7e16d60f443ee0864ea80f84c9115b162ed
Elliot Saba <staticfloat@gmail.com>
2020-09-10 03:43:07 +0000

    fix doctests

--- base/binaryplatforms.jl
+++ base/binaryplatforms.jl
@@ -475,12 +475,12 @@ Get the target triplet for the given `Platform` object as a `String`.
 # Examples
 ```jldoctest
 julia> triplet(Platform("x86_64", "MacOS"))
-"x86_64-apple-darwin14"
+"x86_64-apple-darwin"
 
 julia> triplet(Platform("i686", "Windows"))
 "i686-w64-mingw32"
 
-julia> triplet(Platform("armv7l", "Linux"; libgfortran_version="3")
+julia> triplet(Platform("armv7l", "Linux"; libgfortran_version="3"))
 "armv7l-linux-gnueabihf-libgfortran3"
 ```
 """
